b77b67541b508a841debb97b201eae01225607fb
Bruno Macena <bruno.macena@cwi.com.br>
2021-02-13 11:52:00 -0300

    Exercicio 1 aula 2

--- exercicios-aula-02/src/exercicioUm/Filme.java
+++ exercicios-aula-02/src/exercicioUm/Filme.java
@@ -8,53 +8,9 @@ public class Filme {
     private int anoDeLancamento;
 
     public Filme() {
-        this.nomeFilme = nomeFilme;
-        this.descricao = descricao;
-        this.duracao = duracao;
-        this.anoDeLancamento = anoDeLancamento;
-        this.avaliacao = avaliacao;
-    }
 
-    public class Diretor {
-
-        private String nomeDiretor;
-
-        public Diretor(String nomeDiretor, int idadeDiretor, int quantidadeDeFilmesDirigidos) {
-            this.nomeDiretor = nomeDiretor;
-            this.idadeDiretor = idadeDiretor;
-            this.quantidadeDeFilmesDirigidos = quantidadeDeFilmesDirigidos;
         }
 
-        //Get e Set Diretor*****************************************************
-        private int idadeDiretor;
-
-        public String getNomeDiretor() {
-            return nomeDiretor;
-        }
-
-        public void setNomeDiretor(String nomeDiretor) {
-            this.nomeDiretor = nomeDiretor;
-        }
-
-        public int getIdadeDiretor() {
-            return idadeDiretor;
-        }
-
-        public void setIdadeDiretor(int idadeDiretor) {
-            this.idadeDiretor = idadeDiretor;
-        }
-
-        public int getQuantidadeDeFilmesDirigidos() {
-            return quantidadeDeFilmesDirigidos;
-        }
-
-        public void setQuantidadeDeFilmesDirigidos(int quantidadeDeFilmesDirigidos) {
-            this.quantidadeDeFilmesDirigidos = quantidadeDeFilmesDirigidos;
-        }
-
-        private int quantidadeDeFilmesDirigidos;
-
-    }
     ///Get e Set Filme ***************************************************
 
     public String getNomeFilme() {
@@ -95,6 +51,45 @@ public class Filme {
 
     public void setAvaliacao(double avaliacao) {
         this.avaliacao = avaliacao;
+    }
+
+    public class Diretor {
+
+        private String nomeDiretor;
+        private int idadeDiretor;
+        private int quantidadeDeFilmesDirigidos;
+
+
+        public Diretor() {
+
+        }
+
+        //Get e Set Diretor*****************************************************
+
+        public String getNomeDiretor() {
+            return nomeDiretor;
+        }
+
+        public void setNomeDiretor(String nomeDiretor) {
+            this.nomeDiretor = nomeDiretor;
+        }
+
+        public int getIdadeDiretor() {
+            return idadeDiretor;
+        }
+
+        public void setIdadeDiretor(int idadeDiretor) {
+            this.idadeDiretor = idadeDiretor;
+        }
+
+        public int getQuantidadeDeFilmesDirigidos() {
+            return quantidadeDeFilmesDirigidos;
+        }
+
+        public void setQuantidadeDeFilmesDirigidos(int quantidadeDeFilmesDirigidos) {
+            this.quantidadeDeFilmesDirigidos = quantidadeDeFilmesDirigidos;
+        }
+
     }
 
     public void reproduzir(String nomeFilme, String descricao, String duracao, String nomeDiretor) {
